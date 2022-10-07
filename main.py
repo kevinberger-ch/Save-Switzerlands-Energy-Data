be992f9a6b70d85cadad1300f6669ee3e994e241
Kevin Berger <kevin.berger@mcs.ch>
2022-10-07 20:07:01 +0200

    Add run scheduled

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import requests
+import schedule
+import time
 
+RUN_EVERY = 15 # minutes
 API_URL = "https://www.swissgrid.ch/bin/services/apicache?path=/content/swissgrid/de/home/operation/grid-data/current-data/jcr:content/parsys/livedatawidget_copy"
 
-response = requests.get(API_URL)
-if response.status_code == 200:
-    print("Get data successfully")
-    data = response.json()
-    print(data)
-else:
-    print("Request failed!")
-    print("With response status:", response.status_code)
+def getData():
+    response = requests.get(API_URL)
+    if response.status_code == 200:
+        print("Get data successfully")
+        data = response.json()
+        print(data)
+    else:
+        print("Request failed!")
+        print("With response status:", response.status_code)
+
+schedule.every(RUN_EVERY).minutes.do(getData)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
